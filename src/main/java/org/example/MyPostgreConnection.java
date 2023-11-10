6ca6591da02468830f7798beb898754c9b1d7582
Pollyaa <95504556+Pollyaa@users.noreply.github.com>
2023-11-10 16:54:46 +0200

    New version

---
+++ src/main/java/org/example/MyPostgreConnection.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.sql.Connection;
+import org.example.IConnection;
+
+public class MyPostgreConnection implements IConnection {
+    private Connection pgConn = null;
+    public MyPostgreConnection (Connection pgConnection) {
+        pgConn = pgConnection;
+    }
+}
+
+

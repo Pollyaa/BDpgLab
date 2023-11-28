e386eb351b25910afe4b84ae6ee9c2fc4a15aeb5
Pollyaa <95504556+Pollyaa@users.noreply.github.com>
2023-11-28 22:15:32 +0200

    CursorUpdate

--- src/main/java/org/example/ICursor.java
+++ src/main/java/org/example/ICursor.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public interface ICursor {
+    Integer getRecordCount();
+    Integer getColumnsCount();
+    String getColumnName(Integer index);
+    boolean first();
+    boolean last();
+    int getRow();
+    boolean next();
+    boolean previous();
+    String getValue(Integer column);
+    String getValue(String column);
 }
+
--- src/main/java/org/example/MySQLiteCursor.java
+++ src/main/java/org/example/MySQLiteCursor.java
@@ -1,4 +1,114 @@
 package org.example;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MySQLiteCursor implements ICursor {
+    private ResultSet resultSet;
+
+    public MySQLiteCursor(ResultSet resultSet) {
+        this.resultSet = resultSet;
+    }
+
+    @Override
+    public Integer getRecordCount() {
+        int count = 0;
+        try {
+            resultSet.last();
+            count = resultSet.getRow();
+            resultSet.beforeFirst();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    @Override
+    public Integer getColumnsCount() {
+        try {
+            return resultSet.getMetaData().getColumnCount();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @Override
+    public String getColumnName(Integer index) {
+        try {
+            return resultSet.getMetaData().getColumnName(index);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean first() {
+        try {
+            return resultSet.first();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean last() {
+        try {
+            return resultSet.last();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public int getRow() {
+        try {
+            return resultSet.getRow();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean next() {
+        try {
+            return resultSet.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean previous() {
+        try {
+            return resultSet.previous();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public String getValue(Integer column) {
+        try {
+            return resultSet.getString(column);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public String getValue(String column) {
+        try {
+            return resultSet.getString(column);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
